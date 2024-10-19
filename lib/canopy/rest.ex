354bfaddd0333a9d63d2ebe53d2a3597d8dddeb4
Thomas Silva <thomas.silva@anglepoint.com>
2024-10-19 17:31:31 -0600

    include user agent headers

--- lib/canopy/rest.ex
+++ lib/canopy/rest.ex
@@ -1,24 +1,30 @@
 defmodule Canopy.Rest do
+  @default_headers [
+    {~c"User-Agent", ~c"Elixir"}
+  ]
+
   def get_json(url, headers) do
     Application.ensure_all_started([:inets, :ssl])
 
-    :httpc.request(:get, {String.to_charlist(url), headers}, [], []) |> handle_json_result()
+    :httpc.request(:get, {String.to_charlist(url), headers ++ @default_headers}, [], [])
+    |> handle_json_result()
   end
 
-  @application_json String.to_charlist("Application/Json")
+  @application_json ~c"Application/Json"
   def post_json(url, headers, body) do
     Application.ensure_all_started([:inets, :ssl])
 
     :httpc.request(
       :post,
-      {String.to_charlist(url), headers, @application_json, :json.encode(body)},
+      {String.to_charlist(url), headers ++ @default_headers, @application_json,
+       :json.encode(body)},
       [],
       []
     )
     |> handle_json_result()
   end
 
-  def handle_json_result(result) do
+  defp handle_json_result(result) do
     case result do
       {:ok, {{_, 200, _}, _, body}} ->
         json = to_string(body) |> :json.decode()
