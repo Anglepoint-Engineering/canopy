0802abb07244f95e012c7b839becf1d6604f7f13
Thomas Silva <thomas.silva@anglepoint.com>
2024-10-19 21:03:53 -0600

    early debugging

--- lib/mix/tasks/canopy/cover.ex
+++ lib/mix/tasks/canopy/cover.ex
@@ -3,6 +3,7 @@ defmodule Mix.Tasks.Canopy.Cover do
 
   @shortdoc "Test cross coverage of all umbrella apps together."
 
+  require Logger
   alias Canopy.Storage
   alias Canopy.Coverage.Line
   alias Canopy.Coverage.Node
@@ -17,10 +18,11 @@ defmodule Mix.Tasks.Canopy.Cover do
 
     # Collect and generate coverage report
     {:result, coverage_data, _} = :cover.analyse(:coverage, :line)
+    Logger.debug("captured coverage for: #{length(coverage_data)} LOC")
 
     coverage_data
     |> Line.lines_from_coverage()
-    |> dbg()
+    |> tap(&Logger.debug("parsed coverage for: #{map_size(&1)} module(s)"))
     |> Storage.persist!("line_coverage")
     |> Node.node_tree_from_lines()
     |> Storage.persist!("node_coverage")
@@ -30,6 +32,7 @@ defmodule Mix.Tasks.Canopy.Cover do
 
   defp compile_umbrella_apps do
     Mix.Dep.Umbrella.loaded()
+    |> tap(&Logger.debug("compiling test coverage for: #{length(&1)} app(s)"))
     |> Enum.map(&(Path.join(&1.opts[:build], "ebin") |> String.to_charlist()))
     |> Enum.each(&:cover.compile_beam_directory(&1))
   end
