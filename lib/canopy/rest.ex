b2230f40204f92947f22692e49257108bbe48ab8
Thomas Silva <thomas.silva@anglepoint.com>
2024-10-19 20:02:10 -0600

    fix rest

--- lib/canopy/rest.ex
+++ lib/canopy/rest.ex
@@ -32,9 +32,12 @@ defmodule Canopy.Rest do
 
   defp handle_json_result(result) do
     case result do
-      {:ok, {{_, 200..201, _}, _, body}} ->
+      {:ok, {{_, 200, _}, _, body}} ->
         json = to_string(body) |> :json.decode()
         {:ok, json}
+
+      {:ok, {{_, 201, _}, _, _body}} ->
+        {:ok, nil}
 
       {:ok, {{_, status_code, _}, _, body}} ->
         message = "GitHub API returned status #{status_code}: #{body}."
